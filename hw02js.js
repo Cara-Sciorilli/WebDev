681f9671b8ce8c1f1e14e746e4b67127704e6c82
carasciorilliisp <csciorilli@ispecimen.com>
2019-01-17 20:48:53 -0500

    Fixed state after +=

--- hw02js.js
+++ hw02js.js
@@ -1,50 +1,56 @@
 (function(){
-var state = {currentSol: 0, operation: "", nextNum: ""}
+  var state = {currentSol: 0, operation: "", nextNum: ""}
 
-function init() {
-  var buttons = document.getElementsByClassName("button")
-  for(i=0; i<buttons.length; i++){
-    buttons[i].addEventListener("click", function() {myCalcFunction(this);});
+  function init() {
+    var buttons = document.getElementsByClassName("button")
+    for(i=0; i<buttons.length; i++){
+      buttons[i].addEventListener("click", function() {myCalcFunction(this);});
+    }
   }
-}
 
-function myCalcFunction(button) {
-  var name = button.innerHTML;
-  console.log(state);
-  if(name == "C"){
-    state.currentSol = 0;
-    state.nextNum = "";
-    state.operation = "";
-    document.getElementById("display").innerHTML = state.currentSol;
-  }
-  else if (name == "+=" || name == "-" || name == "*" || name == "/"){
-    let parsed = eval(state.nextNum);
-    if(state.operation == "+=") {
-      state.currentSol += parsed;
-    }
-    else if(state.operation == "-"){
-      state.currentSol -= parsed;
-    }
-    else if(state.operation == "*"){
-      state.currentSol *= parsed;
-    }
-    else if(state.operation == "/"){
-      state.currentSol /= parsed;
-    }
-    else if(state.operation == "") {
-      state.currentSol = parsed;
-    }
-    state.operation = name;
-    state.nextNum = "";
-    if(name == "+="){
+  function myCalcFunction(button) {
+    var name = button.innerHTML;
+    console.log(state);
+    if(name == "C"){
+      state.currentSol = 0;
+      state.nextNum = "";
+      state.operation = "";
       document.getElementById("display").innerHTML = state.currentSol;
     }
+    else if (name == "+=" || name == "-" || name == "*" || name == "/"){
+      let parsed = eval(state.nextNum);
+      if(state.operation == "+=") {
+        state.currentSol += parsed;
+      }
+      else if(state.operation == "-"){
+        state.currentSol -= parsed;
+      }
+      else if(state.operation == "*"){
+        state.currentSol *= parsed;
+      }
+      else if(state.operation == "/"){
+        state.currentSol /= parsed;
+      }
+      else if(state.operation == "") {
+        state.currentSol = parsed;
+      }
+      state.operation = name;
+      state.nextNum = "";
+      if(name == "+="){
+        state.nextNum = "0";
+        document.getElementById("display").innerHTML = state.currentSol;
+      }
+    }
+    else {
+      if(state.nextNum == "0" && name != ".") {
+        state.nextNum = name;
+      }
+      else {
+        state.nextNum += name;
+      }
+      document.getElementById("display").innerHTML = state.nextNum;
+    }
   }
-  else {
-    state.nextNum += name;
-    document.getElementById("display").innerHTML = state.nextNum;
-  }
-}
-//Delay the setup code until page is fully loaded.
-window.addEventListener('DOMContentLoaded', init, false);
+  //Delay the setup code until page is fully loaded.
+  window.addEventListener('DOMContentLoaded', init, false);
 })();
